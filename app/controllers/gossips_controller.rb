4785d468967d4978ea46fba14a4c0d92654caac3
larrygold <larry@goldstein.fr>
2018-05-01 17:16:12 +0200

    mardi 17h16

---
+++ app/controllers/gossips_controller.rb
@@ -0,0 +1,40 @@
+class GossipsController < ApplicationController
+
+  def new
+    @gossip = Gossip.new
+  end
+
+  def index
+    @gossips = Gossip.all
+  end
+
+  def create
+    @gossip = Gossip.create(params_gossip)
+    redirect_to gossip_path(@gossip.id)
+  end
+
+  def edit
+    @gossip = Gossip.find(params[:id])
+  end
+
+  def show
+    @gossip = Gossip.find(params[:id])
+  end
+
+  def update
+    @gossip = Gossip.find(params[:id])
+    @gossip.update(params_gossip)
+    redirect_to gossip_path(@gossip.id)
+  end
+
+  def destroy
+    @gossip = Gossip.find(params[:id])
+    @gossip.destroy
+    redirect_to gossips_path
+  end
+
+  def params_gossip
+    params.require(:gossip).permit(:anonymous_author, :content)
+  end
+
+end
